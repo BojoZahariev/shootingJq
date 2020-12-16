72af24a9d99d0746295508c5ec716cb2be85ad34
Bojo <bozhidar_zahariev@hotmail.com>
2020-12-16 10:32:43 +0000

    random num

--- script.js
+++ script.js
@@ -3,8 +3,15 @@ $(document).ready(function() {
     $(this).hide();
 
     setTimeout(() => {
-      $(this).css('left', '120px');
+      $(this).css('left', randomIntFromInterval(0, 1000) + 'px');
+      $(this).css('top', randomIntFromInterval(0, 800) + 'px');
       $(this).show();
     }, 2000);
   });
 });
+
+//random number
+const randomIntFromInterval = (min, max) => {
+  // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
