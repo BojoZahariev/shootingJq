0483ad1149af45ecf8edd0afa987aad3508572f2
Bojo <bozhidar_zahariev@hotmail.com>
2020-12-24 16:22:53 +0000

    timeout hide

--- script.js
+++ script.js
@@ -1,45 +1,37 @@
-
-
-$(document).ready(function() {
-
-  $('#t1').click(function(e) {
-
+$(document).ready(function () {
+  $('#t1').click(function (e) {
     //checks if there are cartridges
-    if($("#cartridges p").hasClass("show")) {
-     
-    var posX = e.pageX - $(this).offset().left;
-    var posY = e.pageY - $(this).offset().top;
+    if ($('#cartridges p').hasClass('show')) {
+      var posX = e.pageX - $(this).offset().left;
+      var posY = e.pageY - $(this).offset().top;
 
-    $('#gun')[0].play();
-    $('#hole').show();
-    $('#hole').css({ left: posX, top: posY });
-   
-    $('#cartridges .show:last').addClass("hide");
-    $('#cartridges .show:last').removeClass("show");
-    $('#cartridges .hide:last').hide();
-    $('#cartridges .hide:last').removeClass("hide");
+      $('#gun')[0].play();
+      $('#hole').show();
+      $('#hole').css({ left: posX, top: posY });
 
-    setTimeout(() => {
-      $('#gun')[0].pause()
-      $('#gun')[0].currentTime = 0;
-      $(this).hide();
-      $('#hole').hide();
-      $(this).css({ top: randomIntFromInterval(40, $(document).height() - 80) + 'px', left: randomIntFromInterval(40, $(document).width() - 80) + 'px' });
-      $(this).show();
-    }, 500);
-   }else{
-  
-    $("#cartridges").append($("<p id='reloadText'></p>").text('Click to reload')); 
+      $('#cartridges .show:last').addClass('hide');
+      $('#cartridges .show:last').removeClass('show');
+      $('#cartridges .hide:last').hide();
+      $('#cartridges .hide:last').removeClass('hide');
 
-   }
+      setTimeout(() => {
+        $('#gun')[0].pause();
+        $('#gun')[0].currentTime = 0;
+        $(this).hide();
+        $('#hole').hide();
+        $(this).css({ top: randomIntFromInterval(40, $(document).height() - 80) + 'px', left: randomIntFromInterval(40, $(document).width() - 80) + 'px' });
+        $(this).show();
+      }, 200);
+    } else {
+      $('#cartridges').append($("<p id='reloadText'></p>").text('Click to reload'));
+    }
   });
-
 });
 
 //Reload
-$('#cartridges').click(function(e) {
+$('#cartridges').click(function (e) {
   $('#reloadText').remove();
-  $('#cartridges .cart').addClass("show");
+  $('#cartridges .cart').addClass('show');
   $('#cartridges .cart').show();
 });
 
@@ -48,4 +40,3 @@ const randomIntFromInterval = (min, max) => {
   // min and max included
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
-
